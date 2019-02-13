b9632adea12b71602ce480e65ade2bafe66b1fff
Dylan Bryan <dylan.bryan@consensys.net>
2019-02-12 20:37:34 -0500

    Inital wiring of tab navigation; should rename some dashboard components

--- android/app/src/main/java/edu/ncsu/csc/assist/MainActivity.java
+++ android/app/src/main/java/edu/ncsu/csc/assist/MainActivity.java
@@ -1,29 +1,84 @@
 package edu.ncsu.csc.assist;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // int id of current tab id
+    private int currentTab;
+
+    // reference to current fragment if we need it
+    private Fragment fragment;
+    // on click listener for bottom nav
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            if (currentTab == item.getItemId()) {
+                return true;
+            }
+
+            switch (item.getItemId()) {
+                case R.id.home_tab:
+                    System.out.println("HOME_TAB");
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.hr_tab:
+                    System.out.println("HR_TAB");
+                    fragment = new HeartRateFragment();
+                    break;
+                case R.id.hrv_tab:
+                    System.out.println("HRV_TAB");
+                    fragment = new HRVFragment();
+                    break;
+                case R.id.o3_tab:
+                    System.out.println("o3_TAB");
+                    fragment = new EnvFragment();
+                    break;
+            }
+            currentTab = item.getItemId();
+            return loadFragment(fragment);
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.dashboard);
+        BottomNavigationView navigation = findViewById(R.id.main_nav);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        fragment = new HomeFragment();
+        loadFragment(fragment);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
