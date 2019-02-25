0221a1645038bf386913cf591440f0a8cb0e3609
Tony Pappas <techtony96@gmail.com>
2019-02-24 19:11:27 -0500

    Generate JSON data for REST requests

--- android/app/src/main/java/edu/ncsu/csc/assist/data/cloud/RestQueue.java
+++ android/app/src/main/java/edu/ncsu/csc/assist/data/cloud/RestQueue.java
@@ -15,7 +15,12 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import edu.ncsu.csc.assist.data.sqlite.entities.RawDataPoint;
 
@@ -48,9 +53,71 @@ public class RestQueue {
 
 
     protected static JSONObject formatJson(List<RawDataPoint> data) throws JSONException {
-        //TODO transform data into JSON
-        return new JSONObject("{size: " + data.size() + "}");
+        JSONObject json = new JSONObject();
+
+        // Get a list of the unique data types to save
+        Set<String> dataTypes = new HashSet<>();
+        for (RawDataPoint raw : data) {
+            dataTypes.add(raw.getType());
+        }
+
+        // Generate sub objects for each type
+        for (String dataType : dataTypes) {
+            List<RawDataPoint> filteredData = new ArrayList<>();
+            for (RawDataPoint raw : data) {
+                if (dataType.equals(raw.getType()))
+                    filteredData.add(raw);
+            }
+            json.put(dataType, genereateSubJson(filteredData));
+        }
+        return json;
     }
+
+    /**
+     * Generate the subsection of JSON that contains the initial timestamp, delta timestamps, and data arrays
+     *
+     * @param rawDataPoints Data all of a single type that you want to be compressed into the above format.
+     * @return
+     * @throws JSONException
+     */
+    protected static JSONObject genereateSubJson(List<RawDataPoint> rawDataPoints) throws JSONException {
+        if (rawDataPoints.size() <= 0) {
+            return null;
+        }
+
+        JSONObject json = new JSONObject();
+
+        // Sort the received data by timestamp
+        Collections.sort(rawDataPoints, new SortByTimestamp());
+
+        // Find the lowest timestamp in the given data
+        long initialTimestamp = rawDataPoints.get(0).getTimestamp();
+        json.put("initial_timestamp", initialTimestamp);
+
+        long[] delta = new long[rawDataPoints.size() - 1];
+        int[] data = new int[rawDataPoints.size()];
+
+        long lastTimeStamp = initialTimestamp;
+        data[0] = rawDataPoints.get(0).getValue();
+        for (int i = 1; i < rawDataPoints.size(); i++) {
+            data[i] = rawDataPoints.get(i).getValue();
+            delta[i - 1] = rawDataPoints.get(i).getTimestamp() - lastTimeStamp;
+            lastTimeStamp = rawDataPoints.get(i).getTimestamp();
+        }
+        json.put("delta", delta);
+        json.put("data", data);
+        return json;
+    }
+
+
+    private static class SortByTimestamp implements Comparator<RawDataPoint> {
+        public int compare(RawDataPoint o1, RawDataPoint o2) {
+            return Long.compare(o1.getTimestamp(), o2.getTimestamp());
+        }
+    }
+
+
+
 
     String getEndpointUrl() {
         return ENDPOINT_URL;
