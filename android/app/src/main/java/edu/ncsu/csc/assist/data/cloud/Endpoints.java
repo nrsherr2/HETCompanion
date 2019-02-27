a1b27090788c7ef15f83123dc546b1b4960c7fc3
Dylan Bryan <dylan.bryan@consensys.net>
2019-02-26 20:50:55 -0500

    Change Endpoints Constants from interface to class with static fields

--- android/app/src/main/java/edu/ncsu/csc/assist/data/cloud/Endpoints.java
+++ android/app/src/main/java/edu/ncsu/csc/assist/data/cloud/Endpoints.java
@@ -3,9 +3,9 @@ package edu.ncsu.csc.assist.data.cloud;
 /**
  * Holds constants for different cloud endpoints.
  */
-public interface Endpoints {
+public class Endpoints {
     //TODO replace this endpoint with real domain
-    String DOMAIN = "https://example.com";
-    String API_V1 = DOMAIN + "/api/v1";
-    String V1_SAVE = API_V1 + "/save";
+    public static final String DOMAIN = "https://example.com";
+    public static final String API_V1 = DOMAIN + "/api/v1";
+    public static final String V1_SAVE = API_V1 + "/save";
 }
