b089ad05062bc2333f29038ec2a48380dcd9f0c0
Tony Pappas <techtony96@gmail.com>
2019-03-05 10:31:52 -0500

    Fix JSON tests with updated json format

--- android/app/src/test/java/edu/ncsu/csc/assist/data/cloud/JsonUtilTest.java
+++ android/app/src/test/java/edu/ncsu/csc/assist/data/cloud/JsonUtilTest.java
@@ -30,7 +30,7 @@ public class JsonUtilTest {
 
     @Test
     public void testFormatJson() throws Exception {
-        final String expectedJson = "{\"chest_ecg\":{\"initial_timestamp\":0,\"data\":[10,11,12,13,14],\"delta\":[5,5,5,5]},\"chest_ppg\":{\"initial_timestamp\":0,\"data\":[100,101,102,103,104],\"delta\":[6,6,6,6]},\"chest_inertia_x\":{\"initial_timestamp\":0,\"data\":[1000,1001,1002,1003,1004],\"delta\":[7,7,7,7]}}";
+        final String expectedJson = "{\"user_id\":\"0\",\"chest_ecg\":{\"initial_timestamp\":0,\"data\":[10,11,12,13,14],\"delta\":[5,5,5,5]},\"het_version\":\"0.1\",\"chest_ppg\":{\"initial_timestamp\":0,\"data\":[100,101,102,103,104],\"delta\":[6,6,6,6]},\"chest_inertia_x\":{\"initial_timestamp\":0,\"data\":[1000,1001,1002,1003,1004],\"delta\":[7,7,7,7]}}";
 
         List<RawDataPoint> data = new ArrayList<>(25);
 
@@ -45,11 +45,11 @@ public class JsonUtilTest {
             data.add(new RawDataPoint(DataType.CHEST_INERTIA_X, i * 7, 1000 + i));
         }
 
-        assertThat(JsonUtil.formatJson(data).toString(0), equalTo(expectedJson));
+        assertThat(JsonUtil.formatJson("0", "0.1", data).toString(0), equalTo(expectedJson));
     }
 
     @Test
     public void testFormatJson_NoData() throws Exception {
-        assertThat(JsonUtil.formatJson(new ArrayList<RawDataPoint>()).toString(0), equalTo("{}"));
+        assertThat(JsonUtil.formatJson("0", "0.1", new ArrayList<RawDataPoint>()).toString(0), equalTo("{\"user_id\":\"0\",\"het_version\":\"0.1\"}"));
     }
 }
