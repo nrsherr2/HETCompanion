9f65a22a047b56953245ba56de87343c98784661
Tony Pappas <techtony96@gmail.com>
2019-03-05 11:12:05 -0500

    Fix data distributor test

--- android/app/src/androidTest/java/edu/ncsu/csc/assist/data/device/DataDistributorTest.java
+++ android/app/src/androidTest/java/edu/ncsu/csc/assist/data/device/DataDistributorTest.java
@@ -4,17 +4,26 @@ import org.junit.Test;
 
 import java.util.Random;
 
+import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import edu.ncsu.csc.assist.data.cloud.DataStorer;
 import edu.ncsu.csc.assist.data.cloud.DataUploader;
+import edu.ncsu.csc.assist.data.sqlite.AppDatabase;
+import edu.ncsu.csc.assist.data.sqlite.entities.ConfigOption;
 
 public class DataDistributorTest {
 
     @Test
     public void testDataDistributor() throws Exception{
+        AppDatabase db = Room.databaseBuilder(ApplicationProvider.getApplicationContext(), AppDatabase.class, "ASSIST").build();
+        db.clearAllTables();
+        db.configOptionDao().insert(new ConfigOption("config_user_id", "0"));
+        db.configOptionDao().insert(new ConfigOption("config_het_version", "0.1"));
+        db.configOptionDao().insert(new ConfigOption("user_0_ts_delta", "0"));
+
         final int CHEST_BYTES = 32;
         final int WRIST_BYTES = 28;
-        final int NUM_PACKAGES = 10;
+        final int NUM_PACKAGES = 1;
         final long timestampStart = 5000;
         final long timestampDelta = 20;
         Random gen = new Random();
--- android/app/src/main/java/edu/ncsu/csc/assist/data/cloud/Endpoints.java
+++ android/app/src/main/java/edu/ncsu/csc/assist/data/cloud/Endpoints.java
@@ -5,7 +5,7 @@ package edu.ncsu.csc.assist.data.cloud;
  */
 public interface Endpoints {
     //TODO replace this endpoint with real domain
-    String DOMAIN = "http://152.7.73.22:5555";
+    String DOMAIN = "http://localhost";
     String API_V1 = DOMAIN + "/api/v1";
     String V1_SAVE = API_V1 + "/save";
 }
