4ffdf6afb5c906e8dc7354011afdf071a254a808
Evan Murphy <ermurph6@ncsu.edu>
2019-03-19 10:36:53 -0400

    Changed data processing to handle data streams individually

--- android/app/src/androidTest/java/edu/ncsu/csc/assist/data/device/DataDistributorTest.java
+++ android/app/src/androidTest/java/edu/ncsu/csc/assist/data/device/DataDistributorTest.java
@@ -21,8 +21,10 @@ public class DataDistributorTest {
         db.configOptionDao().insert(new ConfigOption("config_het_version", "0.1"));
         db.configOptionDao().insert(new ConfigOption("user_0_ts_delta", "0"));
 
-        final int CHEST_BYTES = 32;
-        final int WRIST_BYTES = 28;
+        final int CHEST_STREAM_ONE_BYTES = 20;
+        final int CHEST_STREAM_TWO_BYTES = 12;
+        final int WRIST_STREAM_TWO_BYTES = 16;
+        final int WRIST_STREAM_ONE_BYTES = 12;
         final int NUM_PACKAGES = 1;
         final long timestampStart = 5000;
         final long timestampDelta = 20;
@@ -31,14 +33,20 @@ public class DataDistributorTest {
         DataStorer storer = new DataStorer(ApplicationProvider.getApplicationContext());
         DataDistributor distributor = new DataDistributor(storer);
 
-        byte[] chestData = new byte[CHEST_BYTES];
-        byte[] wristData = new byte[WRIST_BYTES];
+        byte[] chestStreamOneData = new byte[CHEST_STREAM_ONE_BYTES];
+        byte[] chestStreamTwoData = new byte[CHEST_STREAM_TWO_BYTES];
+        byte[] wristStreamOneData = new byte[WRIST_STREAM_ONE_BYTES];
+        byte[] wristStreamTwoData = new byte[WRIST_STREAM_TWO_BYTES];
 
         for(int i = 0; i < NUM_PACKAGES; i++){
-            gen.nextBytes(chestData);
-            gen.nextBytes(wristData);
-            distributor.distributeChestData(chestData, timestampStart + i*timestampDelta);
-            distributor.distributeWristData(wristData, timestampStart + i*timestampDelta);
+            gen.nextBytes(chestStreamOneData);
+            gen.nextBytes(chestStreamTwoData);
+            gen.nextBytes(wristStreamOneData);
+            gen.nextBytes(wristStreamTwoData);
+            distributor.distributeChestStreamOne(chestStreamOneData, timestampStart + i * timestampDelta);
+            distributor.distributeChestStreamTwo(chestStreamTwoData, timestampStart + i * timestampDelta);
+            distributor.distributeWristStreamOne(wristStreamOneData, timestampStart + i * timestampDelta);
+            distributor.distributeWristStreamTwo(wristStreamTwoData, timestampStart + i * timestampDelta);
         }
 
         storer.flush();
--- android/app/src/main/java/edu/ncsu/csc/assist/data/device/DataDistributor.java
+++ android/app/src/main/java/edu/ncsu/csc/assist/data/device/DataDistributor.java
@@ -25,28 +25,37 @@ import edu.ncsu.csc.assist.data.handling.WristPpgHandler;
  */
 public class DataDistributor {
 
+    //Chest Stream 1
     private Handler chestEcgHandler;
-    private Handler chestInertialHandler;
     private Handler chestPpgHandler;
 
-    private Handler wristEnvironmentalHandler;
+    //Chest Stream 2
+    private Handler chestInertialHandler;
+
+    //Wrist Stream 1
     private Handler wristInertialHandler;
-    private Handler wristOzoneHandler;
     private Handler wristPpgHandler;
 
-    //many other handlers will go here
+    //Wrist Stream 2
+    private Handler wristOzoneHandler;
+    private Handler wristEnvironmentalHandler;
 
-    //once other handlers are created, the below values will only be initialized through the constructor
+
+    //once  handlers are created, the below values will overwritten through the appropriate constructor
     private int CHEST_ECG_BYTES = 12;
     private int CHEST_PPG_BYTES = 8;
+    private int CHEST_STREAM_ONE = CHEST_ECG_BYTES + CHEST_PPG_BYTES;
+
     private int CHEST_INERTIAL_BYTES = 12;
-    private int CHEST_DATA_BYTES = CHEST_ECG_BYTES + CHEST_PPG_BYTES + CHEST_INERTIAL_BYTES;
+    private int CHEST_STREAM_TWO = CHEST_INERTIAL_BYTES;
 
     private int WRIST_INERTIAL_BYTES = 12;
     private int WRIST_PPG_BYTES = 4;
+    private int WRIST_STREAM_ONE = WRIST_INERTIAL_BYTES + WRIST_PPG_BYTES;
+
     private int WRIST_OZONE_BYTES = 8;
     private int WRIST_ENVIRONMENTAL_BYTES = 4;
-    private int WRIST_DATA_BYTES = WRIST_INERTIAL_BYTES + WRIST_PPG_BYTES + WRIST_OZONE_BYTES + WRIST_ENVIRONMENTAL_BYTES;
+    private int WRIST_STREAM_TWO = WRIST_OZONE_BYTES + WRIST_ENVIRONMENTAL_BYTES;
 
     public DataDistributor(DataStorer rawDataBuffer) {
         chestEcgHandler = new ChestEcgHandler(rawDataBuffer);
@@ -66,14 +75,37 @@ public class DataDistributor {
         WRIST_ENVIRONMENTAL_BYTES = wristEnvironmentalHandler.getTotalByteSize();
     }
 
-    public void distributeChestData(byte[] data, long timestamp) {
+//    public void distributeChestData(byte[] data, long timestamp) {
+//
+//        if (data.length != CHEST_DATA_BYTES) {
+//            throw new IllegalArgumentException("HET Chest data received did not match expected length");
+//        }
+//        int offset = 0;
+//
+//        //--CHEST DATA PARSING--
+//        //ECG data
+//        byte[] ecgData = Arrays.copyOfRange(data, offset, offset + CHEST_ECG_BYTES);
+//        offset += CHEST_ECG_BYTES;
+//        chestEcgHandler.handle(ecgData, timestamp);
+//
+//        //PPG data
+//        byte[] ppgData = Arrays.copyOfRange(data, offset, offset + CHEST_PPG_BYTES);
+//        offset += CHEST_PPG_BYTES;
+//        chestPpgHandler.handle(ppgData, timestamp);
+//
+//        //Inertial data
+//        byte[] inertialData = Arrays.copyOfRange(data, offset, offset + CHEST_INERTIAL_BYTES);
+//        offset += CHEST_INERTIAL_BYTES;
+//        chestInertialHandler.handle(inertialData,timestamp);
+//    }
 
-        if (data.length != CHEST_DATA_BYTES) {
-            throw new IllegalArgumentException("HET Chest data received did not match expected length");
+    public void distributeChestStreamOne(byte[] data, long timestamp) {
+        if (data.length != CHEST_STREAM_ONE) {
+            throw new IllegalArgumentException("HET Chest data stream one received did not match expected length");
         }
         int offset = 0;
 
-        //--CHEST DATA PARSING--
+        //--CHEST STREAM ONE DATA PARSING--
         //ECG data
         byte[] ecgData = Arrays.copyOfRange(data, offset, offset + CHEST_ECG_BYTES);
         offset += CHEST_ECG_BYTES;
@@ -83,21 +115,58 @@ public class DataDistributor {
         byte[] ppgData = Arrays.copyOfRange(data, offset, offset + CHEST_PPG_BYTES);
         offset += CHEST_PPG_BYTES;
         chestPpgHandler.handle(ppgData, timestamp);
+    }
 
+    public void distributeChestStreamTwo(byte[] data, long timestamp) {
+        if (data.length != CHEST_STREAM_TWO) {
+            throw new IllegalArgumentException("HET Chest data stream two received did not match expected length");
+        }
+        int offset = 0;
+
+        //--CHEST STREAM TWO DATA PARSING--
         //Inertial data
         byte[] inertialData = Arrays.copyOfRange(data, offset, offset + CHEST_INERTIAL_BYTES);
         offset += CHEST_INERTIAL_BYTES;
         chestInertialHandler.handle(inertialData,timestamp);
     }
 
-    public void distributeWristData(byte[] data, long timestamp) {
 
-        if (data.length != WRIST_DATA_BYTES) {
-            throw new IllegalArgumentException("HET Wrist data received did not match expected length");
+//    public void distributeWristData(byte[] data, long timestamp) {
+//
+//        if (data.length != WRIST_DATA_BYTES) {
+//            throw new IllegalArgumentException("HET Wrist data received did not match expected length");
+//        }
+//        int offset = 0;
+//
+//        //--WRIST DATA PARSING--
+//        //Inertial data
+//        byte[] inertialData = Arrays.copyOfRange(data, offset, offset + WRIST_INERTIAL_BYTES);
+//        offset += WRIST_INERTIAL_BYTES;
+//        wristInertialHandler.handle(inertialData,timestamp);
+//
+//        //PPG data
+//        byte[] ppgData = Arrays.copyOfRange(data, offset, offset + WRIST_PPG_BYTES);
+//        offset += WRIST_PPG_BYTES;
+//        wristPpgHandler.handle(ppgData,timestamp);
+//
+//        //ozone data
+//        byte[] ozoneData = Arrays.copyOfRange(data, offset, offset + WRIST_OZONE_BYTES);
+//        offset += WRIST_OZONE_BYTES;
+//        wristOzoneHandler.handle(ozoneData,timestamp);
+//
+//        //environmental data
+//        byte[] environmentalData = Arrays.copyOfRange(data, offset, offset + WRIST_ENVIRONMENTAL_BYTES);
+//        offset += WRIST_ENVIRONMENTAL_BYTES;
+//        wristEnvironmentalHandler.handle(environmentalData, timestamp);
+//    }
+
+    public void distributeWristStreamOne(byte[] data, long timestamp) {
+        if (data.length != WRIST_STREAM_ONE) {
+            throw new IllegalArgumentException("HET Wrist data stream one received did not match expected length");
         }
         int offset = 0;
 
-        //--WRIST DATA PARSING--
+        //--WRIST STREAM ONE DATA PARSING--
         //Inertial data
         byte[] inertialData = Arrays.copyOfRange(data, offset, offset + WRIST_INERTIAL_BYTES);
         offset += WRIST_INERTIAL_BYTES;
@@ -107,13 +176,21 @@ public class DataDistributor {
         byte[] ppgData = Arrays.copyOfRange(data, offset, offset + WRIST_PPG_BYTES);
         offset += WRIST_PPG_BYTES;
         wristPpgHandler.handle(ppgData,timestamp);
+    }
 
-        //ozone data
+    public void distributeWristStreamTwo(byte[] data, long timestamp) {
+        if (data.length != WRIST_STREAM_TWO) {
+            throw new IllegalArgumentException("HET Wrist data stream two received did not match expected length");
+        }
+        int offset = 0;
+
+        //--WRIST STREAM TWO DATA PARSING--
+        //Ozone data
         byte[] ozoneData = Arrays.copyOfRange(data, offset, offset + WRIST_OZONE_BYTES);
         offset += WRIST_OZONE_BYTES;
         wristOzoneHandler.handle(ozoneData,timestamp);
 
-        //environmental data
+        //Environmental data
         byte[] environmentalData = Arrays.copyOfRange(data, offset, offset + WRIST_ENVIRONMENTAL_BYTES);
         offset += WRIST_ENVIRONMENTAL_BYTES;
         wristEnvironmentalHandler.handle(environmentalData, timestamp);
